0487e4d343cf2509d65b0e46f8beabe31c776647
Leandro Martini <martinileandro12@gmail.com>
2025-12-10 20:40:44 -0300

    exercicios de funções agregadas

---
+++ exercicios-função-agregada.sql
@@ -0,0 +1,43 @@
+select idVendedor, avg(valor) from pedido group by idVendedor having avg(valor) > 200;
+
+select idVendedor, sum(valor) from pedido group by idVendedor having sum(valor) > 1500;
+
+select idVendedor, sum(valor) from pedido group by idVendedor;
+
+select count(idMunicipio) from municipio;
+
+select count(idMunicipio) from municipio where idUf = 1 or idUf = 2;
+
+select idUf, count(idMunicipio) from municipio group by idUf;
+
+select count(idCliente) from cliente where logradouro is not null;
+
+select idMunicipio, count(idCliente) from cliente group by idMunicipio;
+
+select count(idFornecedor) from fornecedor;
+
+select idFornecedor, count(idProduto) from produto group by idFornecedor;
+
+select avg(valor) from produto where idFornecedor = 1;
+
+select sum(valor) from produto;
+
+select nome, valor from produto order by valor desc limit 1;
+
+select nome, valor from produto order by valor asc limit 1;
+
+select avg(valor) from produto;
+
+select count(idTransportadora) from transportadora;
+
+select avg(valor) from pedido;
+
+select idCliente, sum(valor) from pedido group by idCliente;
+
+select idVendedor, sum(valor) from pedido group by idVendedor;
+
+select idTransportadora, sum(valor) from pedido group by idTransportadora;
+
+
+
+
