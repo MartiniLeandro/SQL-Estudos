c2cd17d5a814e1448b967e33b7c5d02485a0f18e
Leandro Martini <martinileandro12@gmail.com>
2025-12-18 21:16:28 -0300

    exercicio view

--- aprendendo-view.sql
+++ aprendendo-view.sql
@@ -7,3 +7,30 @@ select cliente from cliente_profissao where profissao = 'Professor';
 drop  view cliente_profissao;
 
 -- caso queira atualizar a view, é recomendado apaga-la e criar novamente
+
+-- EXERCÍCIOS
+create view infos_client as -- falta UF
+	cln.nome,
+	prf.nome as profissao,
+	ncl.nome as nacionalidade,
+	cmp.nome as complemento,
+	mcp.nome as municipio,
+	brr.nome as bairro,
+	cln.cpf, cln.rg,
+	cln.data_nascimento,
+	cln.genero,
+	cln.logradouro,
+	cln.numero,
+	cln.observacoes
+from
+	cliente as cln
+left outer join
+	profissao as prf on cln.idProfissao = prf.idProfissao
+left outer join
+	nacionalidade as ncl on cln.idNacionalidade = ncl.idNacionalidade
+left outer join
+	complemento as cmp on cln.idComplemento = cmp.idComplemento
+left outer join
+	municipio as mcp on cln.idMunicipio = mcp.idMunicipio
+left outer join
+	bairro as brr on cln.idBairro = brr.idBairro;
