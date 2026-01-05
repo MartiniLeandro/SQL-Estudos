20bfdecaa8995f79f715c7c48b722f5ccff30fff
Leandro Martini <martinileandro12@gmail.com>
2026-01-04 21:52:54 -0300

    exercícios simples função agregada

--- exercicios-diversos.sql
+++ exercicios-diversos.sql
@@ -6,3 +6,13 @@ select * from livro_treino where editora is null;
 select * from livro_treino where titulo like 'O%';
 select * from livro_treino order by ano_lancamento asc;
 select * from livro_treino order by titulo;
+
+select count(*) from livro_treino;
+select count(*) from livro_treino where disponivel = true;
+select count(*) from livro_treino where editora is null;
+select count(distinct autor) from livro_treino;
+select min(ano_lancamento) from livro_treino;
+select max(ano_lancamento) from livro_treino;
+select avg(ano_lancamento) from livro_treino;
+select editora, count(*) from livro_treino group by editora;
+select editora, avg(ano_Lancamento) from livro_treino group by editora;
